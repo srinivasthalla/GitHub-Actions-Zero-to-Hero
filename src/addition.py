77d805669ab934c0d0053229ebe280bfc9f8b06f
Srinivas Thalla <157224356+srinivasthalla@users.noreply.github.com>
2025-11-04 17:28:11 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -2,7 +2,6 @@
 def add(a, b):
     return a + b
 
-if __name__ == "__main__":
-    num1 = 5
-    num2 = 7
-    print("Sum:", add(num1, num2))
+def test_add():
+    assert add(1, 2) == 3
+    assert add(1, -1) == 0
